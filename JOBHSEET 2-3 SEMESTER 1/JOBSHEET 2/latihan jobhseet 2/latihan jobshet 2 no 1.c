38d28d00b9223876b56b8b1dc86068d1b67ef45c
serlyekaputri <145112732+serlyekaputri@users.noreply.github.com>
2024-03-22 20:46:04 +0700

    Delete JOBHSEET 2-3 SEMESTER 1 directory

--- JOBHSEET 2-3 SEMESTER 1/JOBSHEET 2/latihan jobhseet 2/latihan jobshet 2 no 1.c
+++
@@ -1,7 +0,0 @@
-//created by serly eka putri_23343083
-#include <stdio.h>
-
-int main(){
-	// kode atau logika program kita di sini
-	return 0;
-}
--- JOBHSEET 2-3 SEMESTER 1/JOBSHEET 2/latihan jobhseet 2/latihan jobshet 2 no 6.c
+++
@@ -1,9 +0,0 @@
-//created by serly eka putri_23343083
-#include <stdio.h>
-
-int main(){
-	putchar("A");
-	
-	return 0;
-	
-}
