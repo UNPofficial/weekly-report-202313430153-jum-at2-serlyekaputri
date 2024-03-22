bb0efbcdb326ac7dbdb987a92fd67bef3fd24364
serlyekaputri <145112732+serlyekaputri@users.noreply.github.com>
2024-03-22 20:37:53 +0700

    Add files via upload

---
+++ JOBSHEET 8-9 SEMESTER 1/JOBSHEET 8/latihan jobshet 8/latihan jobshet 8 no 1.c
@@ -0,0 +1,12 @@
+// created by serly eka putri_23343083
+#include <stdio.h>
+
+// membuat fungsi say_hello
+void say_hello(){
+	printf("Hello selamat datang!\n");
+}
+void main(){
+	// memanggil fungsi say_hello
+	say_hello();
+	
+}
