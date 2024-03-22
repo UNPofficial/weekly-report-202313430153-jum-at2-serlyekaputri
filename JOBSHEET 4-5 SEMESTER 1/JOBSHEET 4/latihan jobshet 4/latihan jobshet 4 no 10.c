12ac2b39a0858485a61b7052017276b22d6347bc
serlyekaputri <145112732+serlyekaputri@users.noreply.github.com>
2024-03-22 20:31:55 +0700

    Add files via upload

---
+++ JOBSHEET 4-5 SEMESTER 1/JOBSHEET 4/latihan jobshet 4/latihan jobshet 4 no 10.c
@@ -0,0 +1,8 @@
+//created by serly eka putri_23343083
+#include <stdio.h>
+
+void main(){
+	int a = 6; 
+	
+	printf("~a = %d\n", ~a);
+}
---
+++ JOBSHEET 4-5 SEMESTER 1/JOBSHEET 4/latihan jobshet 4/latihan jobshet 4 no 2.c
@@ -0,0 +1,20 @@
+//created by serly eka putri_23343083
+#include <stdio.h>
+
+void main(){
+	
+	int a, b;
+	
+	printf("Inputkan nilai a: ");
+	scanf("%i", &a);
+	
+	printf("Inputkan nilai b: ");
+	scanf("%i", &b);
+	
+	printf("Hasil a + b: %i\n", a + b);
+	printf("Hasil a - b: %i\n", a - b);
+	printf("Hasil a * b: %i\n", a * b);
+	printf("Hasil a / b: %i\n", a / b);
+	printf("Hasil a % b: %i\n", a % b);
+	
+} 
